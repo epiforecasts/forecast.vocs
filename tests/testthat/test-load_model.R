f7d585bc91e5e0d5ee3807ef863f2c662204f704
seabbs <s.e.abbott12@gmail.com>
2021-10-10 17:30:01 +0000

    add tests for load_model

---
+++ tests/testthat/test-load_model.R
@@ -0,0 +1,31 @@
+
+test_that("Returns paths to models", {
+  single <- load_model(strains = 1, compile = FALSE)
+  expect_type(single, "character")
+  expect_true(grepl("stan/bp.stan", single))
+  two <- load_model(strains = 2, compile = FALSE)
+  expect_type(two, "character")
+  expect_true(grepl("stan/twostrainbp.stan", two))
+})
+
+test_that(
+  "Compilation of the single strain models is successful and syntax is valid",
+  {
+    single <- suppressMessages(load_model(strains = 1, compile = TRUE))
+    single <- suppressMessages(cmdstanr::cmdstan_model(single))
+    expect_error(suppressMessages(single$check_syntax()), NA)
+  }
+)
+
+test_that(
+  "Compilation of two strain model is successful and syntax is valid",
+  {
+    two <- suppressMessages(load_model(strains = 2, compile = TRUE))
+    two <- suppressMessages(cmdstanr::cmdstan_model(two))
+    expect_error(suppressMessages(two$check_syntax()), NA)
+  }
+)
+
+test_that("Cannot load a model that doesn't exist", {
+  expect_error(load_model(strains = 3))
+})
