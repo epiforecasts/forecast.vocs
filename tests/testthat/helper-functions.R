72845df379075c8c88e2b908c1eba73e72777a95
seabbs <s.e.abbott12@gmail.com>
2021-10-10 23:14:44 +0000

    stan fit tests

---
+++ tests/testthat/helper-functions.R
@@ -0,0 +1,42 @@
+on_ci <- function() {
+  isTRUE(as.logical(Sys.getenv("CI")))
+}
+
+not_on_cran <- function() {
+  on_ci() || identical(Sys.getenv("NOT_CRAN"), "true")
+}
+
+silent_stan_fit <- function(...) {
+  utils::capture.output(
+    fit <- suppressMessages(stan_fit(...))
+  )
+  return(fit)
+}
+
+test_stan_fit <- function(message, dt, model, inits) {
+  test_that(message, {
+    skip_on_cran()
+    fit <- silent_stan_fit(
+      data = dt, model = model, init = inits, chains = 2, adapt_delta = 0.9,
+      max_treedepth = 15, refresh = 0, show_messages = FALSE,
+      iter_warmup = 1000, iter_sampling = 1000
+    )
+    expect_type(fit, "list")
+    expect_true(data.table::is.data.table(fit))
+    expect_equal(nrow(fit), 1)
+    expect_named(
+      fit,
+      expected = c(
+        "fit", "data", "fit_args", "samples", "max_rhat",
+        "divergent_transitions", "per_divergent_transitons", "max_treedepth",
+        "no_at_max_treedepth", "per_at_max_treedepth"
+      )
+    )
+    expect_equal(class(fit$fit[[1]])[1], "CmdStanMCMC")
+    expect_lt(fit$per_divergent_transitons, 0.1)
+    expect_lt(fit$max_treedepth, 15)
+    expect_lt(fit$max_rhat, 1.1)
+    expect_type(fit$fit_args[[1]], "list")
+    expect_type(fit$data[[1]], "list")
+  })
+}
---
+++ tests/testthat/test-stan_fit.R
@@ -0,0 +1,85 @@
+options(mc.cores = 2)
+
+if (not_on_cran()) {
+  obs <- filter_by_availability(
+    germany_covid19_delta_obs,
+    date = "2021-06-26"
+  )
+
+  overdisp_dt <- stan_data(
+    obs,
+    overdispersion = TRUE, variant_relationship = "pooled",
+    voc_scale = c(0.4, 0.2)
+  )
+  nooverdisp_dt <- stan_data(obs, overdispersion = FALSE)
+  scaled_dt <- stan_data(
+    obs,
+    overdispersion = TRUE, variant_relationship = "scaled",
+    voc_scale = c(0.4, 0.2)
+  )
+  independent_dt <- stan_data(
+    obs,
+    overdispersion = TRUE, variant_relationship = "independent",
+    voc_scale = c(0.4, 0.2)
+  )
+  ndisp_scaled_dt <- stan_data(
+    obs,
+    overdispersion = FALSE, variant_relationship = "scaled",
+    voc_scale = c(0.4, 0.2)
+  )
+  ndisp_independent_dt <- stan_data(
+    obs,
+    overdispersion = FALSE, variant_relationship = "independent",
+    voc_scale = c(0.4, 0.2)
+  )
+  inits1 <- stan_inits(overdisp_dt, strains = 1)
+  inits2 <- stan_inits(overdisp_dt, strains = 2)
+  one_model <- suppressMessages(load_model(strains = 1))
+  two_model <- suppressMessages(load_model(strains = 2))
+}
+
+test_stan_fit(
+  "The single strain model with overdispersion can be fit as expected",
+  overdisp_dt, one_model, inits1
+)
+
+test_stan_fit(
+  "The single strain model without overdispersion can be fit as expected",
+  nooverdisp_dt, one_model, inits1
+)
+
+test_stan_fit(
+  "The two strain model with pooling and overdispersion can be fit as
+   expected",
+  overdisp_dt, two_model, inits2
+)
+
+test_stan_fit(
+  "The two strain model with pooling and without overdispersion can be fit as
+   expected",
+  nooverdisp_dt, two_model, inits2
+)
+
+test_stan_fit(
+  "The two strain model with scaling and overdispersion can be fit as
+   expected",
+  scaled_dt, two_model, inits2
+)
+
+test_stan_fit(
+  "The two strain model with scaling and no overdispersion can be fit as
+   expected",
+  ndisp_scaled_dt, two_model, inits2
+)
+
+test_stan_fit(
+  "The two strain model with independence and overdispersion can be fit as
+   expected",
+  independent_dt, two_model, inits2
+)
+
+test_stan_fit(
+  "The two strain model with independence and no overdispersion can be fit as
+   expected",
+  ndisp_independent_dt, two_model, inits2
+)
