df7d523420ec54b63bdb4b3aaf04c87421b1f178
seabbs <s.e.abbott12@gmail.com>
2021-10-11 13:19:50 +0000

    update arguments

--- tests/testthat/test-summarise_posterior.R
+++ tests/testthat/test-summarise_posterior.R
@@ -1,26 +1,26 @@
 
 test_summarise_posterior(
   "The single strain model can have its posterior summarised as expected",
-  fit1, posterior1, 1,
+  fit1, posterior1, 1, obs,
   equal = TRUE
 )
 
 test_summarise_posterior(
   "The two strain model can have its posterior summarised as expected",
-  fit2, posterior2, 2,
+  fit2, posterior2, 2, obs,
   equal = TRUE
 )
 
 test_summarise_posterior(
   "The two strain model can have its posterior summarised as expected with
    a custom quantile list",
-  fit2, posterior2, 2,
+  fit2, posterior2, 2, obs,
   equal = FALSE, probs = c(0.2, 0.3, 0.6, 0.8)
 )
 
 test_summarise_posterior(
   "The two strain model can have its posterior summarised as expected with
    a custom VOC label",
-  fit2, posterior2, 2,
+  fit2, posterior2, 2, obs,
   equal = FALSE, voc_label = "Delta",
 )
