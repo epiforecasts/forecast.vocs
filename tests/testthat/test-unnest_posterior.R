194258c038299cda44d2e9e02e65aa135e033aa3
Sam Abbott <s.e.abbott12@gmail.com>
2021-10-18 12:28:28 +0100

    Hotfix: Test skipping for universe build

--- tests/testthat/test-unnest_posterior.R
+++ tests/testthat/test-unnest_posterior.R
@@ -1,5 +1,6 @@
 
 test_that("Can unnest a posterior successfully", {
+  skip_on_cran()
   pdt <- data.table::data.table(posterior = list(posterior1, posterior2))
   upost <- unnest_posterior(pdt)
   expect_equal(upost, rbind(posterior1, posterior2))
