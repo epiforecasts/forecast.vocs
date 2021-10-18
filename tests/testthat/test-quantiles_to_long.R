583acba267828e80400433e72f530f4dffb6bdc4
Sam Abbott <s.e.abbott12@gmail.com>
2021-10-18 12:32:53 +0100

    Hotfix: Make quantiles_to_long tests use cache

--- tests/testthat/test-quantiles_to_long.R
+++ tests/testthat/test-quantiles_to_long.R
@@ -1,7 +1,7 @@
 
 test_that("Can convert a posterior to long format successfully", {
-  skip_on_cran()
-  long <- quantiles_to_long(posterior1)
+  p <- fv_example(type = "posterior")
+  long <- quantiles_to_long(p)
   expect_true(all(c("quantile", "prediction") %in% names(long)))
   expect_type(long$quantile, "double")
   expect_equal(unique(long$quantile), c(0.05, 0.20, 0.80, 0.95))
