6cbb7779dc98dc3b3845ba366891f6806d2b1efd
Sam Abbott <contact@samabbott.co.uk>
2021-12-13 17:22:14 +0000

    formatting

--- tests/testthat/test-fv_score_forecast.R
+++ tests/testthat/test-fv_score_forecast.R
@@ -2,7 +2,9 @@
 if (requireNamespace("scoringutils")) {
   test_that("Can score forecasts", {
     skip_on_cran()
-    pp_forecasts <- summary(forecast_wrapper, target = "forecast", type = "cases")
+    pp_forecasts <- summary(
+      forecast_wrapper, target = "forecast", type = "cases"
+    )
 
     expect_data_table(
       fv_score_forecast(pp_forecasts, current_obs, summarise_by = "strain")
